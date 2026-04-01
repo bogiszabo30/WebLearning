51731e10bda880eea8bc3495ed21e4876ffebd32
Szabó Boglárka <bogiszabo30@gmail.com>
2026-04-01 19:23:08 +0200

    fix tailwind

---
+++ Frontend/web-learning/src/index.ts
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import LoginView from './views/LoginView.vue'
+import RegisterView from './views/RegisterView.vue'
+import DashboardView from './views/DashboardView.vue'
+
+const routes = [
+  {
+    path: '/login',
+    name: 'Login',
+    component: LoginView
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: RegisterView
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardView
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
