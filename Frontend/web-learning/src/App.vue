d20b3e45e9e89f5a7a0922de5d18408c97043964
Szabó Boglárka <bogiszabo30@gmail.com>
2026-06-18 18:17:04 +0200

    make navbar conditional

--- Frontend/web-learning/src/App.vue
+++ Frontend/web-learning/src/App.vue
@@ -1,12 +1,18 @@
 <script setup>
-import { RouterView } from 'vue-router'
+import { RouterView, useRoute } from 'vue-router'
 import AppNavBar from './components/AppNavBar.vue'
+import { computed } from 'vue';
+
+const route = useRoute()
+const isLoginOrRegisterRoute = computed(() => {
+  return route.path === '/login' || route.path === '/register'
+})
 </script>
 
 <template>
   <div class="flex flex-col min-h-screen">
 
-    <AppNavBar />
+    <AppNavBar v-if="!isLoginOrRegisterRoute" />
 
     <div class="flex-1 flex items-center justify-center">
       <RouterView></RouterView>
